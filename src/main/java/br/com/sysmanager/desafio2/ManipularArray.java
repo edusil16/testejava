f83c2d74bb9e71605ef5e2c4f7eda8327fb20f8f
Eduardo Santos <edu.sil1606@gmail.com>
2019-07-20 11:51:51 -0300

    Alterações Parciais

--- src/main/java/br/com/sysmanager/desafio2/ManipularArray.java
+++ src/main/java/br/com/sysmanager/desafio2/ManipularArray.java
@@ -1,11 +1,10 @@
 package br.com.sysmanager.desafio2;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ManipularArray {
-    public static void main(String[] args) throws IOException {
+    public void countNumbers(){
         int quantidade = 10;
         int[] vetor = new int[quantidade];
 
@@ -15,22 +14,19 @@ public class ManipularArray {
 
         System.out.println("Array original");
         printArray(vetor);
-        long tempoInicial = System.currentTimeMillis();
 
         insertionSort(vetor);
 
         System.out.println("Array ordenado");
         printArray(vetor);
 
-        long tempoFinal = System.currentTimeMillis();
-
-        System.out.println("Executado em = " + (tempoFinal - tempoInicial) + " ms");
-
+        // Parâmetro que utilizei para comparar o Array criado aleatoriamente de 0 a 10
+        System.out.println("Array comparador com o método");
         printArray(getNumbersLessThan(4,vetor));
 
     }
 
-    public static void insertionSort(int[] vetor) {
+    public void insertionSort(int[] vetor) {
         int j;
         int key;
         int i;
@@ -44,7 +40,7 @@ public class ManipularArray {
         }
     }
 
-    private static Integer[] getNumbersLessThan(int number, int[] array) {
+    private Integer[] getNumbersLessThan(int number, int[] array) {
         List<Integer> lessNumbers=new ArrayList();
         for(int i:array) {
             if(i<number) {
@@ -54,16 +50,16 @@ public class ManipularArray {
         return lessNumbers.toArray(new Integer[lessNumbers.size()]);
     }
 
-    private static void printArray(int[] array) {
-        StringBuilder buf=new StringBuilder("Array: ");
+    private void printArray(int[] array) {
+        StringBuilder buf = new StringBuilder("Array: ");
         for(int i:array) {
             buf.append(i).append(",");
         }
         System.out.println(buf.toString().substring(0,buf.length()-1));
     }
 
-    private static void printArray(Integer[] array) {
-        StringBuilder buf=new StringBuilder("Array: ");
+    private void printArray(Integer[] array) {
+        StringBuilder buf = new StringBuilder("Array: ");
         for(int i:array) {
             buf.append(i).append(",");
         }
